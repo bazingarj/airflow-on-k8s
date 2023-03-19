9af48a5670dfa9472d8c6c5b2e24bb370d97c0ae
RJ <admin@192.168.1.16>
2023-03-20 02:37:29 +0530

    airflow dags example added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # airflow-on-k8s
+
+DOC: https://rahul-juneja3.medium.com/migrate-airflow-from-single-instance-to-k8s-airflow-cluster-83cfb9db1188
---
+++ sample-dag-with-cpu-and-ram.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+import time
+
+def print_hello():
+  time.sleep(30)
+  print('Hello World!')
+  return 'Hello world!'
+
+with DAG('hello_world', description='Simple tutorial DAG', schedule_interval='*/5 * * * *', start_date=datetime(2023, 2, 26), catchup=False):
+
+  hello_operator = PythonOperator(
+    task_id='hello_task', 
+    python_callable=print_hello, 
+    #executor_config={ "KubernetesExecutor": {"request_memory": "100Mi","limit_memory": "200Mi","request_cpu": "0.25","limit_cpu": "0.5"} }
+    executor_config={ "KubernetesExecutor": 
+    { 
+    "resources": {
+            "requests": {
+                "memory": "200Mi",
+                "cpu": "0.2"
+            },
+            "limits": {
+                "memory": "200Mi",
+                "cpu": "0.2"
+            }
+        } 
+    }
+    }
+  )
+
+  hello_operator
+
+
